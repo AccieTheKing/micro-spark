68566177ed06fcaa4d088059b8aa079ceef1007b
Acdaling Edusei <acdaling@gmail.com>
2025-08-27 22:51:24 +0200

    Deploy script adjustments

--- deploy.sh
+++ deploy.sh
@@ -7,7 +7,7 @@ echo "Pulling latest code..."
 git pull origin master
 
 echo "Stop current container"
-docker compose down
+docker compose stop micro-spark
 
 echo "Starting new container"
 docker compose up -d --build
