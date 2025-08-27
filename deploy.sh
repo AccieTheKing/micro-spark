d28722717c725cb23a436fe1719e599dbe8c23f2
Acdaling Edusei <acdaling@gmail.com>
2025-08-27 22:38:08 +0200

    Adjust script

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,3 @@ cd ~/projects/micro-spark
 
 echo "Pulling latest code..."
 git pull origin master
-
-echo "Stop current container"
-docker compose down
-
-echo "Starting new container"
-docker compose up -d --build
